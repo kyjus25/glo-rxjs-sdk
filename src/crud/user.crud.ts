8f756363e86b7bbbc02786da788f327d0e6b6911
Justin White <kyjus25@gmail.com>
2019-06-12 16:59:37 -0500

    User completed

--- src/crud/user.crud.ts
+++ src/crud/user.crud.ts
@@ -1,8 +1,11 @@
 import {RequestService} from '../request.service';
+import {Observable} from 'rxjs/Observable';
+import {User} from '../types/return/user';
+import {UserRequestFields} from '../types/payload/user.request';
 
 export class UserCrud extends RequestService {
-
-  public get() {
-    console.log('set');
+  public get(query?: UserRequestFields[] | string[]): Observable<User> {
+    const queryString = this.buildQueryString(query);
+    return this.request('get', '/user', null, queryString);
   }
 }
---
+++ src/types/payload/user.request.ts
@@ -0,0 +1,7 @@
+
+export enum UserRequestFields {
+  created_date = 'created_date',
+  email = 'email',
+  name = 'name',
+  username = 'username'
+}
