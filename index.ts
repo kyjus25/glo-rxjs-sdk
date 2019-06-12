9d62a900ac21d0f6073a0331330edc4b4cd66f77
Justin White <kyjus25@gmail.com>
2019-06-12 17:05:13 -0500

    export types

--- index.ts
+++ index.ts
@@ -1,2 +1,20 @@
 export {GloSDKModule} from './src/glo-sdk.module';
 export {GloSDK} from './src/glo-sdk';
+
+// Request Types
+export * from './src/types/payload/attachment.request';
+export * from './src/types/payload/board.request';
+export * from './src/types/payload/card.request';
+export * from './src/types/payload/column.request';
+export * from './src/types/payload/comment.request';
+export * from './src/types/payload/label.request';
+export * from './src/types/payload/user.request';
+
+// DTO Types
+export * from './src/types/return/attachment';
+export * from './src/types/return/board';
+export * from './src/types/return/card';
+export * from './src/types/return/column';
+export * from './src/types/return/comment';
+export * from './src/types/return/label';
+export * from './src/types/return/user';
